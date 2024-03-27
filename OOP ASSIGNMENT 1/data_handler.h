31d1cf40d3af07484d898997acc5ad1ed6499d09
msamishahid <146338536+msamishahid@users.noreply.github.com>
2024-03-27 16:23:11 +0500

    added teachers and section vector and function prototypes data_handler.h

--- OOP ASSIGNMENT 1/data_handler.h
+++ OOP ASSIGNMENT 1/data_handler.h
@@ -10,12 +10,19 @@
 #include "section.h"
 
 extern std::vector<student> students_list;
+extern std::vector<teacher> teachers_list;
+extern std::vector<section> sections_list;
 
+const std::string TEACHER_FILE = "teacher.txt";
 const std::string STUDENT_FILE = "student.txt";
 const std::string FILE_PREFIX = "timetable_";
 
-void load_data(std::vector<student>& students_list);
+void load_data(std::vector<student>& students_list, std::vector<teacher>& teachers_list);
 void load_timetable_data();
+void saveTeachers();
+void removeTeacher(const std::string& application_id);
+void displayTeachers();
 void displayTimetable(const student& s);
+void displayTeacherTimetable(const std::string& application_id);
 
 #endif
